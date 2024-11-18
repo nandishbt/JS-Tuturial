35f5b5ac9a25677ca1a047a125563a0e7f455739
nandishbt <104723228+nandishbt@users.noreply.github.com>
2024-11-18 07:58:44 +0000

    added basic datatype and variables

---
+++ JS/Basics_0/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"  // to make sure all js code is of newer version
+
+
+// alert(5+5) -> not allowed in node only in browser
+
+
+//string
+//number
+//bigint
+//boolean
+//undefined -> variabledeclared but not assigned a value
+//null -> empty value || standalone value
+//symbol -> unique
+
+
+//object
+
+
+let a = null; //object type
+let b; // undefined type
+
+console.log(typeof a); //object
+console.log(typeof b); //undefined
+
---
+++ JS/Basics_0/variables.js
@@ -0,0 +1,18 @@
+const userId = 123456;
+//assigned once and we cant change the value
+
+let name = "nandish"
+var age = 25
+let city 
+
+/*
+let  is block scoped and we can declare another variable with same name in other block 
+so it is better to use let rather than var 
+*/
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([userId,name,age,city])
