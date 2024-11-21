12160c2b9b0697fbd5516c45860517e9b2ddfd80
nandishbt <104723228+nandishbt@users.noreply.github.com>
2024-11-21 14:01:14 +0000

    stack and heap memory

--- JS/Basics_0/datatype-summary.js
+++ JS/Basics_0/datatype-summary.js
@@ -30,5 +30,36 @@ console.log(typeof obj); //Object type
 
 
 
+/* 
+//datatypes are based on memory allocation
+Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+These variables are assigned a value and stored in stack memory
+
+Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+
+JavaScript is a dynamically typed language. This means that you don't need to specify the data type of a variable when you declare it.
+
+
+
+
+
+*/
+
+let a = 10; //a is stored in Stack memory holding value 10
+let b = a; //b is also stored in stack which copies the value of a 
+
+let obj = {
+    name:"nandish"}  //obj,obj2 variables is stored in stack memory and actual object is stored in heap obj points to object
+                    //i.e   stack------------------heap
+let obj2 = obj      //       a=10,
+                    //       b=10
+                    //      obj,obj2 ---------------->>>{name:"nandish}"
+                     //               
+
+
+
+
+
 
 
