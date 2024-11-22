9fb7871fdb05790af171223144d9e2396b8f8db9
nandishbt <104723228+nandishbt@users.noreply.github.com>
2024-11-22 09:03:01 +0000

    strings added

---
+++ JS/Basics_0/strings.js
@@ -0,0 +1,45 @@
+// const name = "nandish";
+// console.log(name);
+
+// console.log(name[0]);//n
+
+const newName = new String("harsh")
+console.log(newName);
+
+
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(2, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
