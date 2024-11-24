dcf02bca99157404333719874f699bc7fa0208fb
nandishbt <104723228+nandishbt@users.noreply.github.com>
2024-11-24 07:45:12 +0000

    number and math added

---
+++ JS/Basics_0/number_math_.js
@@ -0,0 +1,14 @@
+let num = 56;
+// console.log(num);
+
+let MyNumber = new Number(55);
+
+// console.log(MyNumber);  //[Number : 55]//
+
+
+console.log(MyNumber.toExponential(2));
+
+console.log(MyNumber.toFixed(2)); //55.00
+
+
+
