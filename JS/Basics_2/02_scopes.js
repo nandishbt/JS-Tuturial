ce5024775aa15c939b37cb09e2be0bfc793465af
nandishbt <104723228+nandishbt@users.noreply.github.com>
2024-11-29 12:07:06 +0000

    added scopes 2

--- JS/Basics_2/02_scopes.js
+++ JS/Basics_2/02_scopes.js
@@ -15,7 +15,40 @@ var c = 30;
 
 if(true){
     
-    console.log(`a = ${a} , b = ${b}, c = ${c}`) //a = 10 , b = 20, c = 30
+    // console.log(`a = ${a} , b = ${b}, c = ${c}`) //a = 10 , b = 20, c = 30
+}
+
+
+//inner functions
+
+function One(){
+    const name = "nandish"
+    function Two(){
+        const age = 23;
+        console.log(name);
+        
+    }
+    // console.log(age); //gives error because age variable not visible outside its block
+
+    Two()
+    
+}
+
+// One()
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) //Gives 6 
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5) //Gives Error
+const addTwo = function(num){
+    return num + 2
 }
 
 
